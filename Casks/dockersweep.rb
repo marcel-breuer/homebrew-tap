e194ac554aa2940f99974dc5bcadc5e7b979bc91
Marcel Breuer <breuer.marcel@outlook.com>
2026-07-24 11:49:59 +0200

    chore(cask): update DockerSweep to 0.1.1

--- Casks/dockersweep.rb
+++ Casks/dockersweep.rb
@@ -1,6 +1,6 @@
 cask "dockersweep" do
-  version "0.1.0"
-  sha256 "c055b60bc9bfe6d0aeb1239daf3c20cf23478c89bae26e8770d36076a5c3c54c"
+  version "0.1.1"
+  sha256 "89309577cdde5fa3bee8baf398ee224a584c1b055a0bab02777538f4680c7688"
 
   url "https://github.com/marcel-breuer/docker-sweep/releases/download/v#{version}/DockerSweep-#{version}-arm64.zip"
   name "DockerSweep"
